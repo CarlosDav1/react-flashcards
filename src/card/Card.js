370cbe0d6cefc6a5eb19216e5a63a33773776ae0
Carlos Gonzalez <carlosdav1@outlook.com>
2021-01-15 10:54:00 -0700

    Added functionality to the card

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,23 +1,45 @@
 import React, { useState } from 'react';
 import './card.css'
 
+function Answer(){
+    return(
+        <>
+            <div className="divisionLine"></div>
+            <p className="answerStyle">This is an answer</p>
+        </>
+    );
+}
+
+function Buttons(){
+    return(
+        <>
+            <div className="buttonContainer">
+                <button className="correct">O</button>
+                <button className="wrong">X</button>
+            </div>
+        </>
+    );
+}
+
 function Card(){
-    const [question, setQuestion] = useState('first question');
+    let [isRevealed, revealAnswer] = useState(false);
+    let a = isRevealed? <Answer />: null;
+    let b = isRevealed? <Buttons />: null;
+
+    function Reveal(){
+        revealAnswer(prevState => prevState = true);
+    }
 
     return (
         <>
-        <div className="cardContainer">
-            <h1 className="questionStyle">{question}</h1>
-            <div className="divisionLine"></div>
-            <p className="answerStyle">This is an answer</p>
+        <div className="cardContainer" onClick={Reveal}>
+            <h1 className="questionStyle">This is a question</h1>
+            {a}
         </div>
 
-        <div className="buttonContainer">
-            <button className="correct">O</button>
-            <button className="wrong">X</button>
-        </div>
+        {b}
         </>
-    );
+    ); 
 }
 
 export default Card;
