c2461afaac0fd99264f89dda7e871e659e9bd85f
Carlos Gonzalez <carlosdav1@outlook.com>
2021-01-15 09:43:40 -0700

    Added style to the card

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import './card.css'
 
 function Card(){
     const [question, setQuestion] = useState('first question');
 
     return (
         <div className="cardContainer">
-            <h1>{question}</h1>
+            <h1 className="questionStyle">{question}</h1>
+            <div className="divisionLine"></div>
+            <p className="answerStyle">This is an answer</p>
         </div>
     );
 }
