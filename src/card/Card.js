2e8cc576664e83384d62ec8ac96bd21cb4ba4712
Carlos Gonzalez <carlosdav1@outlook.com>
2021-01-16 07:25:25 -0700

    Created several components and improved the card's logic as well

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,45 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './card.css'
+import deckDemo from '../deck/deckDemo.json' 
 
-function Answer(){
+function Answer(props){
     return(
         <>
             <div className="divisionLine"></div>
-            <p className="answerStyle">This is an answer</p>
+            <p className="answerStyle">{props.ans}</p>
         </>
     );
 }
 
-function Button(props){return <button onClick={props.toggleEvent}>{props.correctOrNo}</button>;}
+function ButtonDiv(props){
+    return(
+        <>
+            <div className="buttonContainer">
+                <button onClick={props.nqFunc}>X</button>
+                <button onClick={props.nqFunc}>O</button>
+            </div>
+        </>
+    );
+}
+
+function MainComp(props){
+    return(
+        <>
+            <div className="cardContainer" onClick={props.reveal}>
+            <h1 className="questionStyle">{props.question}</h1>
+                {props.answer}
+            </div>
+
+            {props.buttons}
+        </>
+    );
+}
+
+function GoBack(){
+    return(
+        <>
+            <h1>Congratulations! You Finished the deck</h1>
+            <p>do you want to retry or do wanna go back?</p>
+            <button>Retry</button>
+            <button>Go Back</button>
+        </>
+    );
+}
 
 function Card(){
     let [isRevealed, revealAnswer] = useState(false);
+    let [currentQuestion, setQuestion] = useState('');
+    let [currentAnswer, setAnswer] = useState('');
+    let [count, setCount] = useState(0);
     
-    function ToggleReveal(){
-        revealAnswer(prevState => prevState = !prevState);
+    let answer = isRevealed? <Answer ans={currentAnswer}/>: null;
+    let buttonContainer = isRevealed? <ButtonDiv nqFunc={() => NextQuestion(false)}/> : null;
+
+    useEffect(() => {NextQuestion(true);}, [])
+
+    function NextQuestion(isFirst){
+        setQuestion(currentQuestion = deckDemo["questions"][count]);
+        setAnswer(currentAnswer = deckDemo["answers"][count]);
+        setCount(prevState => prevState + 1);
+        
+        if(!isFirst){revealAnswer(prevState => prevState = !prevState)}
     }
 
-    let a = isRevealed? <Answer />: null;
-    let b = isRevealed? 
-        <>
-            <div className="buttonContainer">
-                <Button correctOrNo="X" toggleEvent={ToggleReveal}/>
-                <Button correctOrNo="O" toggleEvent={ToggleReveal}/>
-            </div>
-        </>
-        : null;
-
-    
-    return (
-        <>
-        <div className="cardContainer" onClick={ToggleReveal}>
-            <h1 className="questionStyle">This is a question</h1>
-            {a}
-        </div>
-
-        {b}
-        </>
-    ); 
+    return count == deckDemo["questions"].length + 1?
+        <GoBack />
+        :<MainComp reveal={() => revealAnswer(true)} question={currentQuestion} answer={answer} buttons={buttonContainer}/>; 
 }
 
 export default Card;
