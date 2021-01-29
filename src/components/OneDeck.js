36e6d3dd330d19ce5b688839e0c3630a6aa63b1e
Carlos Gonzalez <carlosdav1@outlook.com>
2021-01-29 16:17:39 -0700

    Styled a few things, working with orginizing files

--- src/components/OneDeck.js
+++ src/components/OneDeck.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import trash from '../icons/delete.png';
-import edit from '../icons/edit.png';
-import '../styles/deckComponent.css';
+import '../styles/OneDeck.css';
 import { Link } from 'react-router-dom';
 
 function OneDeck(props){
@@ -9,29 +7,29 @@ function OneDeck(props){
 
     return(
         <div className="deckContainer">
+            <div className="insideDecoration">
+                <Link to={isEmpty}>
+                    <div className="clickArea" onClick={() => {
+                            
+                            if(props.deck.cards.length == 0){alert("This deck is empty")}
+                            else{
+                                props.change(props.deck);
+                            }
 
-            <Link to={isEmpty}>
-                <div className="clickArea" onClick={() => {
-                        
-                        if(props.deck.cards.length == 0){alert("This deck is empty")}
-                        else{
-                            props.change(props.deck);
-                        }
-
-                    }}>
+                        }}>
+                    </div>
+                </Link>
+                <h1>{props.deck.Name}</h1>
+                <p className="description">{props.deck.Description}</p>
+                <p className="numberOfCards">{props.deck.cards.length.toString()}</p>
+                <div className="buttons">
+                    <button className="edit" onClick={() => {props.change(props.deck); props.setIndex(props.ind)}}>
+                        <Link to="/edit">
+                            Edit
+                        </Link>
+                    </button>
+                    <button className="delete" onClick={() => props.delete(props.ind)}>Delete</button>
                 </div>
-            </Link>
-            <h1>{props.deck.Name}</h1>
-            <p className="description">{props.deck.Description}</p>
-            <p className="numberOfCards">{props.deck.cards.length.toString()}</p>
-            <div className="buttons">
-                <button className="edit" onClick={() => {props.change(props.deck); props.setIndex(props.ind)}}>
-                    <Link to="/edit">
-                        <img src={edit}/>
-                    </Link>
-                </button>
-                <button className="delete" onClick={() => props.delete(props.ind)}><img src={trash}/></button>
-
             </div>
         </div>
     );
